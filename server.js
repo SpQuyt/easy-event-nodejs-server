eaefe39badf7b003f97e20e31e5c4254260a2a8e
Trương Anh Quốc <quocta@talent.ominext.com>
2018-11-10 12:50:17 +0700

    initialize

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/';
+
+app.get('/events', (req, res) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, async function (err, db) {
+    if (err) {
+      res.status(400);
+      res.json({message: 'Unable to connect to the mongoDB server. Error:', err});
+    } else {
+      var projectI = db.db("projectI");
+
+      //await this to return a promise
+      let result_users = await projectI.collection("users").find({name: "Th"}).toArray();
+      var query = {user_id: ObjectId(result_users[0]._id)};
+
+      //print the result
+      projectI.collection("events").find(query).toArray(function (err, result) {
+        if (err) throw err;
+        res.json(result);
+      });
+
+      db.close();
+    }
+  });
+});
+
+app.listen(port, () => console.log("Example app listening on port", port));
+
